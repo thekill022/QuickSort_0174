fc4037125ffccb49291c8176235eb4a656e4a2ed
thekill022 <gibfathoni@gmail.com>
2024-03-28 11:34:01 +0700

    pembuatan variabel untuk array, movement data, perbandingan, dan jumlah elemnt array

--- QuickSort_0174/QuickSort_0174.cpp
+++ QuickSort_0174/QuickSort_0174.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
 using namespace std;
 
-int main() {
+int arr[20]; //inisialisasi variabel array untuk menampung nilai
+int cmp_count = 0; //inisialisasi variabel untuk jumlah perbandingan
+int mov_count = 0; //inisialisasi variabel untuk jumlah pergerakan data
+int n; //inisialisasi variabel n untuk panjang elemnt array
+
+
+
+int main()
+{
 
 }
